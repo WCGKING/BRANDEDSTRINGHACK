5589280925477a0fb60ec5cbb7d59c518a0d91ca
BRANDED KING <121561060+WCGKING@users.noreply.github.com>
2024-11-21 21:35:24 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,5 +2,5 @@
 class Config:
     API_ID = "28461830"
     API_HASH = "58935dd717d0205b8051e77205f407fb"
-    TOKEN = "7095925243:AAG9Du3u9k1tqO9438ONGMn9MTin1oUdSLk"
+    TOKEN = ""
     START_PIC = "https://te.legra.ph/file/e1b4148d7a4cd8b0832d9.jpg"
